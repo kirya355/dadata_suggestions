1783d885a0016aeeca77845d5c6fb205cf2e34a1
kirya355 <immigranticemlaw@yandex.ru>
2021-10-29 14:01:59 +0300

    main fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -32,14 +32,16 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(24),
-          child: TextButton(
-            child: Text('Press enter'),
-            onPressed: () {
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "Paste Dadata API token and press enter",
+            ),
+            onSubmitted: (string) {
               Navigator.of(context).push(
                 PageRouteBuilder(
                   opaque: false,
                   pageBuilder: (BuildContext context, a, b) {
-                    return SuggestionsScreen(token: '2d1e723491f103a22380a0c1aae66ff68dbd9596');
+                    return SuggestionsScreen(token: string);
                   },
                 ),
               );
